cc987b2d38f8102369db88769ae5a3b0f0ef186d
fbrault <ovhredirection@gmail.com>
2014-06-04 13:03:42 +0200

    fonction de cout encore plus meilleure !

--- qt/checkers/game.cpp
+++ qt/checkers/game.cpp
@@ -241,9 +241,9 @@ int Game::costFunction2(const Checkerboard & board, COLOR color) {
     int nega = (player->isWhite() && color==WHITE) || (player->isBlack() && color==BLACK) ? 1 : -1 ;
     int nbMinePiece, nbMineKing, nbOpponentPiece, nbOpponentKing ;
     player->scanNumberOfOnBoard(nbMinePiece, nbMineKing, nbOpponentPiece, nbOpponentKing, board) ;
-    int value = (nbMinePiece - nbOpponentPiece)*3 + (nbMineKing - nbOpponentKing)*9 ;
-    value -= ((nbMinePiece + nbMineKing)==0)*20 ;
-    value += ((nbOpponentPiece + nbOpponentKing)==0)*20 ;
+    int value = (nbMinePiece - nbOpponentPiece)*10 + (nbMineKing - nbOpponentKing)*50 ;
+    value -= ((nbMinePiece + nbMineKing)==0)*100 ;
+    value += ((nbOpponentPiece + nbOpponentKing)==0)*100 ;
     for (int x=0 ; x<board.getSize() ; x++) {
         for (int y=0 ; y<board.getSize() ; y++) {
             if (player->isMine(board.getSquare(x,y))) {
